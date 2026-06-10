07da9bb0e7137a105facea3ba5d192d59862ae7d
farleybsd <farley.ti@hotmail.com>
2026-06-10 09:28:57 -0300

    ConcurrentBag
    ConcurrentBag

---
+++ IntegraccesSincronas-HTTP/Examples/ConcurrentBag/Calculos.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConcurrentBag;
+
+public class Calculos
+{
+    private ConcurrentBag<int> _results = new ConcurrentBag<int>();
+
+    public void PerformCalculations(int[] data)
+    {
+        Parallel.ForEach(data, number =>
+        {
+            int result = ComplexCalculation(number);
+            _results.Add(result);
+        });
+    }
+    private int ComplexCalculation(int number)
+    {
+        return number * number; // Exemplo simples de cálculo
+    }
+    public IEnumerable<int> GetResults()
+    {
+        return _results;
+    }
+}
---
+++ IntegraccesSincronas-HTTP/Examples/ConcurrentBag/Program.cs
@@ -0,0 +1,23 @@
+﻿
+/*
+ * CARACTERÍSTICAS:
+ * IDEAL PARA CENÁRIOS ONDE MÚLTIPLAS
+   THREADS PRECISAM ADICIONAR E
+   REMOVER ITENS
+
+    LOCK-FREE PARA OPERAÇÕES DE
+    ADICIONAR E REMOVER ITENS
+
+ */
+
+using ConcurrentBag;
+
+///Exemplo: Suponha que você está executando cálculos em paralelo e precisa armazenar os resultados.
+///
+Calculos calculos = new Calculos();
+
+
+int[] numeros = new int[5] { 1, 2, 3, 4, 5 };
+
+calculos.PerformCalculations(numeros);
+calculos.GetResults().ToList().ForEach(result => Console.WriteLine(result));
